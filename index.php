fe8dd1760d40e45cac456b83d91275c1acadc673
AHMAD <engineerahkhani@gmail.com>
2016-02-27 19:18:15 +0330

    it's pending for boss idea

--- index.php
+++ index.php
@@ -23,260 +23,108 @@
     <title>Asre Majazi Web Design Site</title>
 </head>
 <body>
-
-<!-- Navigation -->
-<!--<a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>-->
-<!--<nav id="sidebar-wrapper">-->
-<!--    <ul class="sidebar-nav">-->
-<!--        <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>-->
-<!--        <li class="sidebar-brand">-->
-<!--            <a href="#top"  onclick = $("#menu-close").click(); > AsreMajazi</a>-->
-<!--        </li>-->
-<!--        <li>-->
-<!--            <a href="#top" onclick = $("#menu-close").click(); >Home</a>-->
-<!--        </li>-->
-<!--        <li>-->
-<!--            <a href="#about" onclick = $("#menu-close").click(); >About</a>-->
-<!--        </li>-->
-<!--        <li>-->
-<!--            <a href="#services" onclick = $("#menu-close").click(); >Services</a>-->
-<!--        </li>-->
-<!--        <li>-->
-<!--            <a href="#portfolio" onclick = $("#menu-close").click(); >Portfolio</a>-->
-<!--        </li>-->
-<!--        <li>-->
-<!--            <a href="#contact" onclick = $("#menu-close").click(); >Contact</a>-->
-<!--        </li>-->
-<!--    </ul>-->
-<!--</nav>-->
-
-<!-- Header -->
-<header id="top" class="header">
-    <div class="text-vertical-center">
-        <h1>طراحی وب سایت | عصر مجازی</h1>
-        <h2> در کوتاه ترین زمان به وب سایتی با بالاترین کیفیت برسید. </h2>
-        <br>
-        <a href="#about" class="btn btn-dark btn-lg">شروع کنید</a>
-    </div>
-</header>
-
-<!-- About -->
-<section id="about" class="about">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <h2>تحلیل، طراحی و پیاده سازی وب سایت دلخواه شما با آخرین تکنولوژی های روز دنیا در کوتاه ترین زمان</h2>
-                <p class="lead">برای آشنایی بیشتر با می توانیید به این لینک مراجعه نمایید.<a target="_blank" href="http://http://asremajazi.com//">وب سایت شرکت</a>.</p>
+<div id="wrapper">
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Logo</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">خانه</a></li>
+                    <li><a href="#">تماس با ما</a></li>
+                    <li><a href="#">درباره ما</a></li>
+                </ul>
             </div>
         </div>
-        <!-- /.row -->
-    </div>
-    <!-- /.container -->
-</section>
+    </nav>
 
-<!-- Services -->
-<!-- The circle icons use Font Awesome's stacked icon classes. For more information, visit http://fontawesome.io/examples/ -->
-<section id="services" class="services bg-primary">
-    <div class="container">
-        <div class="row text-center">
-            <div class="col-lg-10 col-lg-offset-1">
-                <h2>سفارش سایت های رایج</h2>
-                <hr class="small">
-                <div class="row">
-                    <div class="col-md-3 col-sm-6">
-                        <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-shopping-bag fa-stack-1x text-danger"></i>
-                            </span>
-                            <h4>
-                                <strong>فروشگاهی</strong>
-                            </h4>
-                            <p class="text-justify">شما هم میتوانید وب سایت فروشگاهی داشته باشید.
-                                هم اکنون فروشگاه خود را سفارش دهید و به طور کامل مدیریت نمایید.
-                                اجازه بدهید مغازه های حقیقی کالاهایشان را در فروشگاه شما بفروش برسانند.</p>
-                            <a href="#" class="btn btn-block btn-light">Learn More</a>
-                        </div>
-                    </div>
-                    <div class="col-md-3 col-sm-6">
-                        <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-rss fa-stack-1x text-primary"></i>
-                            </span>
-                            <h4>
-                                <strong>خبری</strong>
-                            </h4>
-                            <p class="text-justify">این روزها همه در فضای آنلاین به دنبال اخبار هستند.
-                                از فرصت استفاده کنید و وب سایت خبری کامل داشته باشید.
-                                اجازه دهید در فضای اینترنت بهتر دیده شوید.</p>
-                            <a href="#" class="btn btn-light btn-block ">Learn More</a>
-                        </div>
-                    </div>
-                    <div class="col-md-3 col-sm-6">
-                        <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-user fa-stack-1x text-warning "></i>
-                            </span>
-                            <h4>
-                                <strong>شخصی</strong>
-                            </h4>
-                            <p class="text-justify">با داشتن وب سایت شخصی بهتر دیده شوید.
-                                میدانید هم اکنون خیلی ها به دنبال شما هستند؟
-                                رزومه خود را آنلاین کنید</p>
-                            <a href="#" class="btn btn-block btn-light">Learn More</a>
-                        </div>
-                    </div>
-                    <div class="col-md-3 col-sm-6">
-                        <div class="service-item">
-                                <span class="fa-stack fa-4x">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+        </ol>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="img/THESUIT_wallpaper.png" alt="Image">
+                <div class="carousel-caption">
+                 <span class="fa-stack fa-4x">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-share-alt text-success fa-stack-1x "></i>
                             </span>
-                            <h4>
-                                <strong>تبلیغات</strong>
-                            </h4>
-                            <p class="text-justify">ما متخصص پیاده سازی وب سایت تبلیغاتی و آگهی هستیم.
-                                مطمئن باشید وب سایت تبلیغاتی شما بازدید کنندگان زیادی را خواهد داشت.
-                                این مطلب کاملا از بهینه سازی سایت های تبلیغاتی ما در گوگل مشخص میباشد.</p>
-                            <a href="#" class="btn  btn-block btn-light">Learn More</a>
-                        </div>
-                    </div>
+                    <h4>
+                        <strong>تبلیغات</strong>
+                    </h4>
+
+                    <p class="text-justify">ما متخصص پیاده سازی وب سایت تبلیغاتی و آگهی هستیم.
+                        مطمئن باشید وب سایت تبلیغاتی شما بازدید کنندگان زیادی را خواهد داشت.
+                        این مطلب کاملا از بهینه سازی سایت های تبلیغاتی ما در گوگل مشخص میباشد.</p>
+                    <a href="#" class="btn  btn-block btn-light">Learn More</a>
                 </div>
-                <!-- /.row (nested) -->
             </div>
-            <!-- /.col-lg-10 -->
-        </div>
-        <!-- /.row -->
-    </div>
-    <!-- /.container -->
-</section>
 
-<!-- Portfolio -->
-<section id="portfolio" class="portfolio">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-10 col-lg-offset-1 text-center">
-                <h2>برخی وب سایت های طراحی شده</h2>
-                <hr class="small">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="portfolio-item">
-                            <a href="#">
-                                <img class="img-portfolio img-responsive" src="img/sample.png">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="portfolio-item">
-                            <a href="#">
-                                <img class="img-portfolio img-responsive" src="img/sample.png">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="portfolio-item">
-                            <a href="#">
-                                <img class="img-portfolio img-responsive" src="img/sample.png">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="portfolio-item">
-                            <a href="#">
-                                <img class="img-portfolio img-responsive" src="img/sample.png">
-                            </a>
-                        </div>
-                    </div>
+            <div class="item">
+                <img src="img/web-development.jpg" alt="Image">
+
+                <div class="carousel-caption">
+                    <h3>More Sell $</h3>
+
+                    <p>Lorem ipsum...</p>
                 </div>
-                <!-- /.row (nested) -->
-                <a href="#" class="btn btn-dark">View More Items</a>
             </div>
-            <!-- /.col-lg-10 -->
         </div>
-        <!-- /.row -->
-    </div>
-    <!-- /.container -->
-</section>
 
-<!-- Footer -->
-<footer>
-    <div class="container">
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="fa fa-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="fa fa-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <div class="container text-center">
+        <h3>What We Do</h3><br>
+
         <div class="row">
-            <div class="col-xs-6">
-                <h3>مراحل کاری تا تنظیم قرارداد</h3>
-                <ul class="list-group">
-                    <li class="list-group-item"><span class="fa fa-2x fa-battery-0 fa-circle">&nbsp;</span> تهیه سند نیازمندیهای وب سایت با مشارکت شما و مهندسان وب شرکت </li>
-                    <li class="list-group-item"><span class="fa fa-2x fa-battery-1 fa-circle">&nbsp;</span> ارائه نمونه اولیه در تایخ مشخص و دریافت نظرات شما</li>
-                    <li class="list-group-item"><span class="fa fa-2x fa-battery-3 fa-circle">&nbsp;</span>بستن قرارداد با توجه به جزئیات مدنظر شما</li>
-                    <li class="list-group-item"><span class="fa fa-2x fa-battery-4 fa-circle">&nbsp;</span>ارائه نسخه نهایی در تاریخ معیین</li>
-                </ul>
-            </div>
-            <div class="col-xs-6 text-center">
-                <h4><strong>ایران - عصر مجازی</strong>
-                </h4>
-                <p>3481 Melrose Place<br>Beverly Hills, CA 90210</p>
-                <ul class="list-unstyled">
-                    <li><i class="fa fa-phone fa-fw"></i> (123) 456-7890</li>
-                    <li><i class="fa fa-envelope-o fa-fw"></i>  <a href="mailto:name@example.com">name@example.com</a>
-                    </li>
-                </ul>
-                <br>
-                <ul class="list-inline">
-                    <li>
-                        <a href="#"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-dribbble fa-fw fa-3x"></i></a>
-                    </li>
-                </ul>
-                <hr class="small">
-                <p class="text-muted">Copyright &copy; AsreMajazi.com | 2016-winter</p>
-            </div>
+            <div class="col-sm-4">
+                <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
 
+                <p>Current Project</p>
+            </div>
+            <div class="col-sm-4">
+                <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+
+                <p>Project 2</p>
+            </div>
+            <div class="col-sm-4">
+                <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+
+                <p>Project 3</p>
+            </div>
         </div>
     </div>
-</footer>
+    <br>
 
+    <footer class="container-fluid text-center">
+        <p class="text-muted">Copyright &copy; AsreMajazi.com | 2016-winter</p>
+    </footer>
+</div>
+<!--wrapper end-->
 <!-- jQuery -->
 <script src="js/html5shiv.js"></script>
 <script src="js/respond.min.js"></script>
 
 <!-- Custom Theme JavaScript -->
-<script>
-    // Closes the sidebar menu
-    $("#menu-close").click(function(e) {
-        e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
-    });
-
-    // Opens the sidebar menu
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
-    });
-
-    // Scrolls to the selected menu item on the page
-    $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    });
-</script>
 
 </body>
 </body>
