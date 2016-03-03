e5c303e29bb62845d07550fea142868f3a4be582
AHMAD <engineerahkhani@gmail.com>
2016-03-03 16:17:03 +0330

    index is almost done

--- index.php
+++ index.php
@@ -19,12 +19,13 @@
 
     <script src="js/bootstrap.min.js"></script>
     <script src="js/bootstrap.rtl.min.js"></script>
+    <script src="http://code.highcharts.com/highcharts.js"></script>
 
     <title>Asre Majazi Web Design Site</title>
 </head>
 <body>
 <div id="wrapper">
-    <nav class="navbar navbar-default navbar-fixed-top">
+    <nav class="navbar navbar-default  navbar-fixed-top">
         <div class="container-fluid">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -36,14 +37,22 @@
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav">
-                    <li > <a href="#"><img id="home" class="bottom"><br>   خانه</a></li>
-                    <li><a href="#"><div id="contactUs"><span class="fa fa-2x fa-envelope"></span></div><br>    تماس با ما</a></li>
-                    <li><a href="#"><div id="aboutUs" ><span class="fa fa-2x fa-rss"></span> </div><br>    درباره ما</a></li>
-                </ul>
+                    <li><a href="#">
+                            <div class="diamond animated slideInLeft  "><span class="fa fa-2x fa-university"></span></div>
+                            <br> صفحه اصلی</a></li>
+                    <li><a href="#">
+                            <div class="diamond"><span class="fa fa-2x fa-suitcase"></span></div>
+                            <br> نمونه کارها</a></li>
+                    <li><a href="#">
+                            <div class="diamond"><span class="fa fa-2x fa-user"></span></div>
+                            <br> درباره ما</a></li>
+                    <li><a href="#">
+                            <div class="diamond"><span class="fa fa-2x fa-envelope"></span></div>
+                            <br> تماس با ما</a></li>
             </div>
         </div>
     </nav>
-
+<div id="contentSec">
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
         <ol class="carousel-indicators">
@@ -53,28 +62,36 @@
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-                <img src="img/THESUIT_wallpaper.png" alt="Image">
-                <div class="carousel-caption">
-                 <span class="fa-stack fa-4x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-share-alt text-success fa-stack-1x "></i>
-                            </span>
-                    <h4>
-                        <strong>تبلیغات</strong>
-                    </h4>
+                <img src="img/bg-green.png" alt="Image">
 
-                    <p class="text-justify">ما متخصص پیاده سازی وب سایت تبلیغاتی و آگهی هستیم.
-                        مطمئن باشید وب سایت تبلیغاتی شما بازدید کنندگان زیادی را خواهد داشت.
-                        این مطلب کاملا از بهینه سازی سایت های تبلیغاتی ما در گوگل مشخص میباشد.</p>
-                    <a href="#" class="btn  btn-block btn-light">Learn More</a>
+                <div class="carousel-caption ">
+                    <h5>
+                        <strong>خدمات ما</strong>
+                    </h5>
+                     <span class="fa-stack fa-2x">
+                            <i class="fa fa-square fa-stack-2x"></i>
+                            <i class="fa fa-share-alt text-success fa-stack-1x "></i>
+                     </span><span class="fa-stack fa-3x">
+                            <i class="fa fa-square fa-stack-2x"></i>
+                            <i class="fa fa-share-alt text-success fa-stack-1x "></i>
+                     </span><span class="fa-stack fa-4x">
+                            <i class="fa fa-square fa-stack-2x"></i>
+                            <i class="fa fa-share-alt text-success fa-stack-1x "></i>
+                     </span></span><span class="fa-stack fa-3x">
+                            <i class="fa fa-square fa-stack-2x"></i>
+                            <i class="fa fa-share-alt text-success fa-stack-1x "></i>
+                     </span></span></span><span class="fa-stack fa-2x">
+                            <i class="fa fa-square fa-stack-2x"></i>
+                            <i class="fa fa-share-alt text-success fa-stack-1x "></i>
+                     </span>
                 </div>
             </div>
 
             <div class="item">
-                <img src="img/web-development.jpg" alt="Image">
+                <img src="img/bg-red2.jpg" alt="Image">
 
                 <div class="carousel-caption">
-                    <h3>More Sell $</h3>
+                    <    ore Sell $</h3>
 
                     <p>Lorem ipsum...</p>
                 </div>
@@ -92,38 +109,153 @@
         </a>
     </div>
 
-    <div class="container text-center">
-        <h3>What We Do</h3><br>
+    <div id="serviceSec" class="container text-center">
+        <h3>سفارش سایت های رایج</h3>
+        <hr class="small">
+        <div class="col-md-3 col-sm-6">
+            <div class="service-item">
+                                <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-shopping-bag fa-stack-1x text-danger"></i>
+                            </span>
+                <h4>
+                    <strong>فروشگاهی</strong>
+                </h4>
 
-        <div class="row">
-            <div class="col-sm-4">
-                <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-
-                <p>Current Project</p>
+                <p class="text-justify">شما هم میتوانید وب سایت فروشگاهی داشته باشید.
+                    هم اکنون فروشگاه خود را سفارش دهید و به طور کامل مدیریت نمایید.
+                    اجازه بدهید مغازه های حقیقی کالاهایشان را در فروشگاه شما بفروش برسانند.</p>
+                <a href="#" class="btn btn-block btn-default">Learn More</a>
             </div>
-            <div class="col-sm-4">
-                <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="service-item">
+                                <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-rss fa-stack-1x text-primary"></i>
+                            </span>
+                <h4>
+                    <strong>خبری</strong>
+                </h4>
 
-                <p>Project 2</p>
+                <p class="text-justify">این روزها همه در فضای آنلاین به دنبال اخبار هستند.
+                    از فرصت استفاده کنید و وب سایت خبری کامل داشته باشید. نظرات و عقاید خود را با دیگران به اشتراک بگذارید.
+                    اجازه دهید در فضای اینترنت بهتر دیده شوید.</p>
+                <a href="#" class="btn btn-default btn-block ">Learn More</a>
             </div>
-            <div class="col-sm-4">
-                <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="service-item">
+                                <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-user fa-stack-1x text-warning "></i>
+                            </span>
+                <h4>
+                    <strong>شخصی</strong>
+                </h4>
 
-                <p>Project 3</p>
+                <p class="text-justify">با داشتن وب سایت شخصی بهتر دیده شوید.
+                    میدانید هم اکنون خیلی ها به دنبال شما هستند؟. این فرصت را از دست ندهید.
+                    رزومه خود را آنلاین کنید تا از این طریق به موقعیت اجتماعی بهتری دست پیدا کنید. </p>
+                <a href="#" class="btn btn-block btn-default">Learn More</a>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="service-item">
+                                <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-share-alt text-success fa-stack-1x "></i>
+                            </span>
+                <h4>
+                    <strong>تبلیغات</strong>
+                </h4>
+
+                <p class="text-justify">ما متخصص پیاده سازی وب سایت تبلیغاتی و آگهی هستیم.
+                    مطمئن باشید وب سایت تبلیغاتی شما بازدید کنندگان زیادی را خواهد داشت.
+                    این مطلب کاملا از بهینه سازی سایت های تبلیغاتی ما در گوگل مشخص میباشد.</p>
+                <a href="#" class="btn  btn-block btn-default">Learn More</a>
             </div>
         </div>
     </div>
     <br>
-<h2>بسته های پیشنهادی؟؟؟؟؟؟</h2>
+
+    <h2>بسته های پیشنهادی؟؟؟؟؟؟</h2>
+    </div>
     <footer class="container-fluid text-center">
+        <div id="highChartContainer" style="width:100%; height:400px;"></div>
         <p class="text-muted">Copyright &copy; AsreMajazi.com | 2016-winter</p>
+
     </footer>
 </div>
 <!--wrapper end-->
-<!-- jQuery -->
-<script src="js/html5shiv.js"></script>
-<script src="js/respond.min.js"></script>
+<script>
+    $(document).ready(function() {
+        alert();
 
+        $(".navbar ul li:nth-child(2)").click(function(){
+            $('contentSec').load('layouts/samples.html')
+        });
+
+        $(".navbar ul li:nth-child(4)").css('animation', 'slideInDown 2.0s');
+        $(".navbar ul li:nth-child(3)").css('animation', 'slideInDown 1.5s');
+        $(".navbar ul li:nth-child(2)").css('animation', 'slideInDown 1.0s');
+        $(".navbar ul li:nth-child(1)").css('animation', 'slideInDown 0.5s');
+        $("#logo").css('animation', "zoomIn 2s");
+
+        $(".service-item .btn-default").mouseover(function(){
+            $(this).css('animation', 'pulse 2s');
+        });
+
+        $(window).scroll(function (event) {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop >= 50) {
+                $(".service-item").css('animation', 'slideInRight 2s');
+            }
+        });
+//high chart
+        $(function () {
+            $('#highChartContainer').highcharts({
+                chart: {
+                    type: 'bar'
+                },
+                title: {
+                    text: 'Fruit Consumption'
+                },
+                xAxis: {
+                    categories: ['Apples', 'Bananas', 'Oranges']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Fruit eaten'
+                    }
+                },
+                series: [{
+                    name: 'Jane',
+                    data: [1, 0, 4]
+                }, {
+                    name: 'John',
+                    data: [5, 7, 3]
+                }]
+            });
+        });
+        var chart1; // globally available
+        $(function() {
+            chart1 = new Highcharts.StockChart({
+                chart: {
+                    renderTo: 'container'
+                },
+                rangeSelector: {
+                    selected: 1
+                },
+                series: [{
+                    name: 'USD to EUR',
+                    data: usdtoeur // predefined JavaScript array
+                }]
+            });
+        });
+
+    });
+</script>
 <!-- Custom Theme JavaScript -->
 
 </body>
